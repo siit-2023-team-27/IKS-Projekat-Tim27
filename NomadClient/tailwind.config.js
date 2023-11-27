6071bac399d0f06b3e6488647202c2f0b15fd453
CodeX-cmd <kuvizic.marko@gmail.com>
2023-11-27 01:23:10 +0100

    [Add] Added all accommodation component

--- NomadClient/tailwind.config.js
+++ NomadClient/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       colors: {
         'nomad-light': '#CDC5B4',
         'nomad-pink': "#B59DA4",
+        'nomad-gray': "#F4F4F4",
+        'text-black': "#525252",
         'nomad-dark': '#551B14'
       }
     },
