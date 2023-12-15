0c585135e357d0d43e536f546c162172501478de
CodeX-cmd <kuvizic.marko@gmail.com>
2023-12-15 23:07:05 +0100

    [Add] implemented account values validation

--- NomadClient/src/app/account/profile/profile.component.ts
+++ NomadClient/src/app/account/profile/profile.component.ts
@@ -12,7 +12,13 @@ export class ProfileComponent {
 
   isModalVisible: boolean = false;
   deleteConformation: boolean = false;
-
+  errors: String[] = [];
+  validationPatterns = {
+    "phoneNumber" : new RegExp("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"),
+    "userName" : new RegExp("[A-Za-z0-9]{3,20}"),
+    "name" : new RegExp("[A-Za-z]{3,20}"),
+    "address" : new RegExp("[A-Za-z0-9]{3,20}"),
+  }
   @Input() user: User = {} as User;
   isEditMode:boolean=false;
   constructor(private authService: AuthService, private service: AccountService) {
@@ -38,12 +44,34 @@ export class ProfileComponent {
     }
   }
   editUser(): void {
+    if(!this.validate()){
+      return;
+    }
     this.service.editUser(this.user).subscribe({
       next: () => {
         
       },
       error: () => { console.log("Error while deleting user with id: ", this.user.id,  "!"); }
     });
+  }
+  validate(): boolean{
+    this.errors = []
+    if(!this.validationPatterns.phoneNumber.test(this.user.phoneNumber)){
+      this.errors.push("Phone number invalid");
+    }
+    if(!this.validationPatterns.userName.test(this.user.username)){
+      this.errors.push("Username invalid");
+    }
+    if(!this.validationPatterns.name.test(this.user.firstName)){
+      this.errors.push("First Name invalid");
+    }
+    if(!this.validationPatterns.name.test(this.user.lastName)){
+      this.errors.push("Last Name invalid");
+    }
+    if(!this.validationPatterns.address.test(this.user.address)){
+      this.errors.push("Address invalid");
+    }
+    return this.errors.length == 0;
   }
   showDialog(): void {
     this.isModalVisible = true;
